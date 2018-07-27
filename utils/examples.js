4714b190afd14d2b16d3593e050b8a5fbe24a85e
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-07-27 00:40:42 -0300

    Add an example of measurement units and functions

--- utils/examples.js
+++ utils/examples.js
@@ -2,5 +2,13 @@
  * Check the 'getExamples' function in order to update the below list
  */
 
-const examples = ['cols.html', 'rows.html', 'template.html', 'displays.html', 'gaps.html'];
+const examples = [
+  'cols.html',
+  'rows.html',
+  'template.html',
+  'displays.html',
+  'gaps.html',
+  'functions.html',
+];
+
 window.examples = examples;
